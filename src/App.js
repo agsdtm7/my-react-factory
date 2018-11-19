037cc042725d4e12b12020acef42d1aefcffd077
agsdtm7 <marsono.a2@gmail.com>
2018-11-19 14:26:41 -0500

    added router and Home component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import TopNavResponsive from './layout/TopNavResponsive';
 import './App.css';
+import Home from './layout/home';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <TopNavResponsive bgColor='#928ABF' txtColor='white' mn3='Products' mnhref3='#products' />
-        <TopNavResponsive totalmenu='3' bgColor='#928ABF' txtColor='white' mn3='service' mnhref3='#service' />
-        <TopNavResponsive totalmenu='3' brand='a' />
-
-        <div className="App">
-          <header className="App-header">
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-
-          </header>
+      <Router>
+        <div>
+          <TopNavResponsive totalmenu='4' brand='a' />
+          <div className="App">
+            <header className="App-header">
+              <Route exact path="/" component={Home} />
+            </header>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/layout/TopNavResponsive.js
+++ src/layout/TopNavResponsive.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Link from 'react-router-dom';
 import bars from '../img/bars.svg';
 import xmenu from '../img/x.svg';
 import brand from '../img/brand.png';
